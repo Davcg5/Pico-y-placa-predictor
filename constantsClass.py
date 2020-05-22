f9b1aa4f29633b8d1fd7cfcb0c60cb74b3c80f5e
David <davcg5@hotmail.com>
2020-05-22 17:11:53 -0500

    class of constants added

---
+++ constantsClass.py
@@ -0,0 +1,11 @@
+class Constants: 
+    
+    TIMEINTERVALS={
+        "Morning": ["07:00", "09:30"], 
+        "Afternoon": ["16:00", "19:30"]
+    }
+    RESTRICTIONSDICTIONARY={"Monday": ["1", "2"], 
+             "Tuesday": ["3", "4"],
+             "Wednesday": ["5", "6"],
+             "Thursday": ["7", "8"],
+             "Friday": ["9", "0"]}
