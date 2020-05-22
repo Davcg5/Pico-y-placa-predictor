b881c076c32df8a13e81fa1a89615cf40edbe43f
David <davcg5@hotmail.com>
2020-05-22 11:58:51 -0500

    validation for motorcycle added

--- plateClass.py
+++ plateClass.py
@@ -8,12 +8,21 @@ class Plate():
 
     def __init__(self, plate): 
         self.plate= plate
-        self.lastDigit=self.plate[-1]
-        
+        self.lastDigit, self.typeOfCar=self.checkTypeOfCar(self.plate)
+
     def getRestrictionDay(self): 
         for key, values in RESTRICTIONSDICTIONARY.items(): 
             if self.lastDigit in values: 
                  return key
 
-       
+    def checkTypeOfCar(self, plate): 
+        if plate[-1].isnumeric(): 
+            lastDigit=plate[-1]
+            typeOfCar="Car"
+        else: 
+            lastDigit=plate[-2]
+            typeOfCar="Motorcycle"
+        return lastDigit, typeOfCar
+    def getTypeOfCar(self): 
+        return self.typeOfCar
 
